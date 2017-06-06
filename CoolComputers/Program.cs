90771c2be308786d39734580855d9d933a13ce49
unknown <Omer>
2017-06-06 15:45:58 -0700

    Initial Commit

---
+++ CoolComputers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoolComputers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
